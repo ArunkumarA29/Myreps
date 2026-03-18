d72802de3b3fe924f3ab9fe1c45512d682e88942
ArunkumarA29 <arunkumarmuruganandham@gmail.com>
2026-03-18 15:54:15 +0530

    Add files via upload

---
+++ Week1Day1.js
@@ -0,0 +1,11 @@
+let number = 9600253261
+let Username = "Arunkumar M"
+let Organisation_name = "Bigbasket"
+let Automationknown = false
+let UsesPlaywright = 
+
+console.log(typeof number + "is :" +number);
+console.log(typeof Username + "is :" +Username);
+console.log(typeof Organisation_name + "is :" +Organisation_name);
+console.log(typeof Automationknown+ "is :" +Automationknown);
+console.log(typeof UsesPlaywright+ "is :" +UsesPlaywright);
